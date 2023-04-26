0179676d3f5ffb4816c87f0d5afdb9979d8e0a04
sasha_kr <a.krutseniuk@gmail.com>
2023-04-26 15:19:22 +0300

    added employees script

---
+++ bash_scripts/employees_list/employees_list.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+declare -A people # declare assosiative array where index=employee name, value=profession
+
+touch employees.txt # create a file to store employees list
+while true; do 
+    read -p "Enter employee's name (or 'done' to finish): " name
+    if [ "$name" == "done" ]; then # cycle works until "done" is printed
+        break
+    fi
+      
+      if ! head -n 1 employees.txt | grep -q -E "Profession|Employee" 2> /dev/null; then # add a header at the top of a file 
+      echo -e "Employee \t\tProfession\n" > employees.txt
+      fi
+
+      if [[ ! $name =~ ^[[:alpha:]]+$ ]]; then # check whether the value contains letters only
+        echo "Name can't contain numbers or symbols (if you're not Musk's XÆA-Ⅻ  )"
+        sleep 0.7
+        continue # go back to the beginning of the loop if provided name is wrong
+      fi
+
+    read -p "Enter employee's profession: " profession
+    people["$name"]=$profession
+    
+    printf "%-16s" "$name" >> employees.txt && printf "%-15s\n" "${people[$name]}" >> employees.txt # add employees data to file
+done
+
+echo -e "\nPeople added:\nName\t\tProfession\n" # print newly added employees
+for name in "${!people[@]}"; do
+  printf "%-16s" "$name" && printf "%-15s\n" "${people[$name]}"
+done
+
+ 
