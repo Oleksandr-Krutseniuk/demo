8201323a463c7fbc7a19a6526bbbe95294f5dc73
sasha_kr <a.krutseniuk@gmail.com>
2023-04-27 20:22:36 +0300

    changes to employees script

--- bash_scripts/employees_list/employees_list.sh
+++ bash_scripts/employees_list/employees_list.sh
@@ -1,123 +1,122 @@
 #!/bin/bash
 
-while true; do
+  
 if [ ! -f employees.txt ]; then # check if employees list file exists
   touch employees.txt # create a file to store employees list if it doesn't exist
 fi
 
+while true; do
+
+declare -A people # create assosiative array where index=employee name, value=profession
+    while read -r column1 column2 column3; do #columns 1 and 2 stands for name+surname, 3 stands for profession
+    name="${column1} ${column2}" #name and surname are combined in 1 var
+    profession="${column3}"
+    people["$name"]="$profession"
+    #printf "%-21s%-20s\n" "$name" "${people[$name]}"
+   done < <(tail -n +3 employees.txt)
+
+    
 # ask user what he's about to do
- 
-  echo -e "\nSelect an option:"
+  
+  echo -e "\nSelect an option:\n"
   echo "1. Add a new employee"
   echo "2. Remove an employee"
   echo "3. Update employee's profession"
-  echo "4. Quit"
+  echo -e "4. Quit\n"
 
-  read -p "Enter option number: " option
-#done
+  read -p $'Enter option number: \n' option
+
+  
 case $option in
   1)
-    declare -A people # create assosiative array where index=employee name, value=profession
-    while true; do 
-    read -p "Enter employee's name (or 'done' to finish): " name
-      if [ "$name" == "done" ]; then # cycle works until "done" is printed
-        break
-      fi
+  declare -A new_people # one more array where input data is stored 
+  while true; do
+    read -p $'Enter employee\'s name and surname (or \'done\' to finish ): \n' first_name second_name
+    
+    if [ "$first_name" == "done" ]; then # cycle works until "done" is printed
+      break
+    fi
+
+    name="${first_name} ${second_name}"
       
-      if ! head -n 1 employees.txt | grep -q -E "Profession|Employee" 2> /dev/null; then # add a header at the top of a file 
-      echo -e "Employee \t\tProfession\n" > employees.txt
-      fi
 
-      if [[ ! $name =~ ^[[:alpha:]]+$ ]]; then # check whether the value contains letters only
-        echo "Name can't contain numbers or symbols (if you're not Musk's XÆA-Ⅻ  )"
-        sleep 0.7
-        continue # go back to the beginning of the loop if provided name is wrong
-      fi
+    if ! head -n 1 employees.txt | grep -q -E "Profession|Employee" 2> /dev/null; then # add a header at the top of a file 
+      printf "%-20s %-22s\n" "Employee" $'Profession\n' > employees.txt
+    fi
 
-      if [[ ${people[$name]} ]]; then # check if name already exists in the associative array
-        echo "The name '$name' already exists in the employee list. Please enter a different name."
-        sleep 0.7
-        continue # go back to the beginning of the loop if name already exists
-      fi
+    if [[ ! $name =~ ^[[:alpha:]]+[[:space:]][[:alpha:]]+$ ]]; then # check whether the value contains letters only
+      echo "Please enter employee's name and surname" # to remake !!!
+      sleep 0.7
+      continue # go back to the beginning of the loop if provided name is wrong
+    fi
 
-    read -p "Enter employee's profession: " profession
-    people["$name"]=$profession
-    printf "%-16s" "$name" >> employees.txt && printf "%-15s\n" "${people[$name]}" >> employees.txt # add employees data to file
+    if [[ ${people["$name"]} ]]; then # check if name already exists in the associative array
+      echo "The name '$name' already exists in the employee list. Please enter a different name."
+      sleep 0.7
+      continue # go back to the beginning of the loop if name already exists
+    else
+      read -p "Enter employee's profession: " profession # добавить переход строки
+      people["$name"]=$profession
+      new_people["$name"]=$profession
+      printf "%-21s" "$name" >> employees.txt && printf "%-20s\n" "${people[$name]}" >> employees.txt # add employees data to file
+    fi
+    
   done
+  sleep 0.7
 
-  echo -e "\nPeople added:\nName\t\tProfession\n" # print newly added employees
-  for name in "${!people[@]}"; do
-    printf "%-16s" "$name" && printf "%-15s\n" "${people[$name]}"
-  done
+  if [ ${#new_people[@]} -eq 0 ]; then
+    sleep 0.7 && echo "No new employees added."
+  else 
+    printf "%-24s" $'\nPeople added:' && printf "%-21s\n" $'Profession\n'
+    for name in "${!new_people[@]}"; do # "!" в цикле позволяет пройтись по ключам вместо значений
+      sleep 0.7
+      printf "%-23s" "$name" && printf "%-20s\n" "${new_people[$name]}"
+    done
+    unset new_people # массив выводит только введенных в этом цикле работников, поэтому его нужно очищать для нового ввода 
+  fi 
     ;;
   
-
   2)
-    declare -A people # create array from existing employees list
-    if [ -f employees.txt ]; then # fill up the array with employees data
-      while read line; do
-      name=$(echo $line | cut -f1 -d $' ') # extract name as index
-      profession=$(echo $line | cut -f2 -d $' ') # extract proffesion as index's value
-      people[$name]=$profession # put the index and value into the array
-      done < <(tail -n +3 employees.txt) # raws with employees start from 3rd string
-    else
-      printf "Employees list file is not found" 
-    fi
 # employee deletion
-    while true; do
-    read -e -p $'Enter the name of the employee you want to remove or press \'exit\' to quit\n' name 
+  while true; do
+  read -e -p $'Enter the name of the employee you want to remove or press \'exit\' to quit\n' name 
 
-      if [ "$name" == "exit" ]; then # cycle works until "done" is printed
-        break
+    if [ "$name" == "exit" ]; then # cycle works until "done" is printed
+      break
       
-      elif [[ ! ${people[$name]} ]]; then
-        echo "The name '$name' does not exist in the employee list."
-        sleep 0.7
-        continue
+    elif [[ ! ${people[$name]} ]]; then
+      echo "The name '$name' does not exist in the employee list."
+      sleep 0.7
+      continue
 
-      else
-        unset people[$name] # remove employee from associative array
-      # remove employee's data from file
-        sed -i "/^$name\s/ d" employees.txt && sleep 0.5
-        echo -e "The employee '$name' has been removed from the employee list\n"
-        continue
-      fi
-    done
-    ;;
-  3)
-    declare -A people # create array from existing employees list
-    if [ -f employees.txt ]; then # fill up the array with employees data
-      while read line; do
-      name=$(echo $line | cut -f1 -d $' ') # extract name as index
-      profession=$(echo $line | cut -f2 -d $' ') # extract proffesion as index's value
-      people[$name]=$profession # put the index and value into the array
-      done < <(tail -n +3 employees.txt) # raws with employees start from 3rd string
     else
-      printf "Employees list file is not found" 
-    fi  
-      
-
+      unset people[$name] # remove employee from associative array
+      # remove employee's data from file
+      sed -i "/^$name\s/ d" employees.txt && sleep 0.5
+      echo -e "The '$name' has been removed from the employee list\n"
+      continue
+    fi
+  done
+  ;;
+  3)
 # profession update
-    
-    while true; do
-    read -e -p $'Enter the name of the employee whose profession you want to update or press \'exit\' to quit\n' name
+  while true; do
+  read -e -p $'Enter the name of the employee whose profession you want to update or press \'exit\' to quit\n' name
+    if [ "$name" == "exit" ]; then # cycle works until "done" is printed
+      break
 
-      if [ "$name" == "exit" ]; then # cycle works until "done" is printed
-        break
-
-      elif [[ ! ${people[$name]} ]]; then # check whether an employee is in the list
-        echo "The name '$name' does not exist in the employee list."
-        sleep 0.7
-        continue # go back to the beginning of the loop if name does not exist
-      else 
-        read -p "Enter new profession for $name employee " new_profession # добавить новую строку
-        people["$name"]=$new_profession # change profession in the associative array
-        #sed -i "s/^$name\s.*/$name\t$new_profession/g" employees.txt # change profession in the file
-        sed -i "s/^$name\s.*/$name\t\t\t$new_profession/g" employees.txt # change profession in the file
-        sleep 0.7
-        echo -e "The profession of '$name' has been changed to '$new_profession'\n"
-        continue
-      fi
+    elif [[ ! ${people[$name]} ]]; then # check whether an employee is in the list
+      echo "The name '$name' does not exist in the employee list."
+      sleep 0.7
+      continue # go back to the beginning of the loop if name does not exist
+    else 
+      read -p "Enter new profession for $name employee"$'\n' new_profession  
+      people["$name"]=$new_profession # change profession in the associative array
+      sed -i "s/^$name\s.*/$name $(printf '%*s' $((19-${#name})) '' ) $new_profession/g" employees.txt
+      sleep 0.7
+      echo -e "The profession of '$name' has been changed to '$new_profession'\n"
+      continue
+    fi
   done
     ;;           
 
